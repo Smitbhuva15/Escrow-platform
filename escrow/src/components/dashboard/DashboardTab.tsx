1f6878dfd72463e3692b39899cd08acd9ae76d3e
Smitbhuva15 <itssmit123@gmail.com>
2025-09-10 23:05:58 +0530

    add Myvoting Section

--- escrow/src/components/dashboard/DashboardTab.tsx
+++ escrow/src/components/dashboard/DashboardTab.tsx
@@ -21,9 +21,9 @@ import { Badge } from "@/components/ui/badge"
 import { Card } from "@/components/ui/card"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "@/store/store"
-import { LoadEscrow } from "@/lib/LoadData"
+import { LoadEscrow, loadTotalVotings } from "@/lib/LoadData"
 import { useActiveAccount } from 'thirdweb/react'
-import { TableCellsMerge } from 'lucide-react'
+import { Loader2, TableCellsMerge } from 'lucide-react'
 
 
 
@@ -33,13 +33,14 @@ export default function DashboardTab() {
   const [activeTab, setActiveTab] = useState("My Deals")
   const [deals, setDeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
+  const [totalVotes, setTotalVotes] = useState<any>([]);
   const dispatch = useDispatch();
 
 
   const escrowContract = useSelector((state: RootState) => state?.escrow?.EscrowContract);
   const provider = useSelector((state: RootState) => state?.escrow?.provider);
   const Deals = useSelector((state: RootState) => state?.escrow?.deals);
-
+  const votes = useSelector((state: RootState) => state?.escrow?.votes);
 
   const isReady = escrowContract && Object.keys(escrowContract).length > 0 &&
     provider && Object.keys(provider).length > 0;
@@ -50,6 +51,7 @@ export default function DashboardTab() {
         setIsLoading(true);
         if (isReady) {
           await LoadEscrow(escrowContract, provider, dispatch);
+          await loadTotalVotings({ dispatch, escrowContract, provider });
         }
       } catch (err) {
         console.log("Failed to load escrow:", err);
@@ -67,7 +69,14 @@ export default function DashboardTab() {
       setDeals(updateDeal)
     }
   }, [Deals, account])
-console.log(deals)
+
+  useEffect(() => {
+    if (votes && votes.length > 0) {
+      const updatevotes = votes.filter((deal: any) => deal?.voterAddress == account?.address)
+      setTotalVotes(updatevotes)
+    }
+  }, [votes, account])
+ 
 
   // const renderStatusBadge = (status: string) => {
   //   switch (status) {
@@ -90,7 +99,7 @@ console.log(deals)
         value={activeTab} onValueChange={setActiveTab}
         className="w-full"
       >
-        <TabsList className="flex-col gap-3 bg-muted/40 p-2 sm:flex-row rounded-xl w-fit">
+        <TabsList className="flex-col gap-3 bg-muted/40 p-2 mt-10  sm:mt-0 sm:flex-row rounded-xl w-fit">
           <TabsTrigger
             value="My Deals"
             className={`rounded-lg px-4 py-2 :bg-primary ${activeTab == "My Deals" && "bg-[#1d45fe]"} p-4  transition`}
@@ -113,141 +122,151 @@ console.log(deals)
         </TabsList>
 
 
-        {/* Account Tab */}
-        <TabsContent value="My Deals" className="sm:mt-6 mt-32" >
-          <Card className="p-4 shadow-md rounded-2xl" >
-            <Table>
-              <TableCaption className="text-muted-foreground">
-                A list of your deals.
-              </TableCaption>
-              <TableHeader>
-                <TableRow className="bg-muted/30">
-                  <TableHead className="w-[120px] font-semibold">#</TableHead>
-                  <TableHead className="font-semibold">title</TableHead>
-                  <TableHead className="font-semibold">Seller</TableHead>
-                  <TableHead className="font-semibold">Amount (ETH)</TableHead>
-                  <TableHead className="text-right font-semibold">Status</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {
-                  deals.map((deal: any,index:number) => (
-                    <TableRow className="hover:bg-muted/20 transition" key={index}>
-                      <TableCell className="w-[120px] font-semibold">{index+1}</TableCell>
-                      <TableCell className="font-semibold capitalize">{deal?.deal?.title}</TableCell>
-                      <TableCell className="font-semibold">{`${deal?.deal?.seller.slice(0, 10)}...${deal?.deal?.seller.slice(-10)}`} </TableCell>
-                      <TableCell className="font-semibold">{`${deal?.deal?.amount/1e18}`} </TableCell>
-                  <TableCell className='text-right' >  <span
-                className={`px-3 py-1 rounded-full text-right text-xs font-medium ${deal?.deal?.status == 1
-                  ? "bg-gray-600 text-white"
-                  : deal?.deal?.status == 2
-                    ? "bg-indigo-500 text-white"
-                    : deal?.deal?.status == 3
-                      ? "bg-slate-400 text-black"
-                      : deal?.deal?.status == 4
-                        ? "bg-emerald-500 text-white"
-                        : deal?.deal?.status == 5
-                          ? "bg-rose-500 text-white"
-                          : "bg-purple-800 text-white"
-                  }`}
-              >
-                {deal?.deal?.status == 1
-                  ? "Created"
-                  : deal?.deal?.status == 2
-                    ? "Funded"
-                    : deal?.deal?.status == 3
-                      ? "Delivered"
-                      : deal?.deal?.status == 4
-                        ? "Confirmed"
-                        : deal?.deal?.status == 5
-                          ? "Disputed"
-                          : "Resolved"}
-              </span></TableCell>
+        {isLoading ? (
+          <div className='h-[65vh] flex justify-center items-center'>
+            <Loader2 className="h-10 w-10 animate-spin text-[white]" />
+          </div>
+        ) : (
+          <>
+            <TabsContent value="My Deals" className="sm:mt-6 mt-32 mb-72" >
+              <Card className="p-4 shadow-md rounded-2xl" >
+                <Table>
+                  <TableCaption className="text-muted-foreground mb-5">
+                    A list of your deals.
+                  </TableCaption>
+                  <TableHeader>
+                    <TableRow className="bg-muted/30">
+                      <TableHead className="w-[120px] font-semibold">#</TableHead>
+                      <TableHead className="font-semibold">title</TableHead>
+                      <TableHead className="font-semibold">Seller</TableHead>
+                      <TableHead className="font-semibold">Amount (ETH)</TableHead>
+                      <TableHead className="text-right font-semibold">Status</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {
+                      deals.map((deal: any, index: number) => (
+                        <TableRow className="hover:bg-muted/20 transition" key={index}>
+                          <TableCell className="w-[120px] font-semibold">{index + 1}</TableCell>
+                          <TableCell className="font-semibold capitalize">{deal?.deal?.title}</TableCell>
+                          <TableCell className="font-semibold">{`${deal?.deal?.seller.slice(0, 10)}...${deal?.deal?.seller.slice(-10)}`} </TableCell>
+                          <TableCell className="font-semibold">{`${deal?.deal?.amount / 1e18}`} </TableCell>
+                          <TableCell className='text-right' >  <span
+                            className={`px-3 py-1 rounded-full text-right text-xs font-medium ${deal?.deal?.status == 1
+                              ? "bg-gray-600 text-white"
+                              : deal?.deal?.status == 2
+                                ? "bg-indigo-500 text-white"
+                                : deal?.deal?.status == 3
+                                  ? "bg-slate-400 text-black"
+                                  : deal?.deal?.status == 4
+                                    ? "bg-emerald-500 text-white"
+                                    : deal?.deal?.status == 5
+                                      ? "bg-rose-500 text-white"
+                                      : "bg-purple-800 text-white"
+                              }`}
+                          >
+                            {deal?.deal?.status == 1
+                              ? "Created"
+                              : deal?.deal?.status == 2
+                                ? "Funded"
+                                : deal?.deal?.status == 3
+                                  ? "Delivered"
+                                  : deal?.deal?.status == 4
+                                    ? "Confirmed"
+                                    : deal?.deal?.status == 5
+                                      ? "Disputed"
+                                      : "Resolved"}
+                          </span></TableCell>
+                        </TableRow>
+
+                      ))
+                    }
+
+
+
+                  </TableBody>
+                  <TableFooter>
+                    <TableRow className="bg-muted/30 font-medium">
+
+                    </TableRow>
+                  </TableFooter>
+                </Table>
+              </Card>
+            </TabsContent>
+
+
+            <TabsContent value="My Votes" className="sm:mt-6 mt-32 mb-72">
+              <Card className="p-4 shadow-md rounded-2xl">
+                <Table>
+                  <TableCaption className="text-muted-foreground mb-5">
+                    A list of my votes.
+                  </TableCaption>
+                  <TableHeader>
+                    <TableRow className="bg-muted/30">
+                      <TableHead className="w-[120px] font-semibold">#</TableHead>
+                      <TableHead className="font-semibold">Vote</TableHead>
+                      <TableHead className="text-right font-semibold">Stake(ETH)</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {
+                      totalVotes.map((vote: any, index: number) => (
+                        <TableRow className="hover:bg-muted/20 transition">
+                          <TableCell className="w-[120px] font-semibold">{index + 1}</TableCell>
+
+                          <TableCell className="font-semibold">{vote.support == true ? "Seller" : "Buyer"}</TableCell>
+                          <TableCell className="text-right font-semibold">{vote.weight/1e18 } </TableCell>
+                        </TableRow>
+                      ))
+                    }
+
+
+                  </TableBody>
+                  <TableFooter>
+                    <TableRow className="bg-muted/30 font-medium">
+
+                    </TableRow>
+                  </TableFooter>
+                </Table>
+              </Card>
+            </TabsContent>
+
+            <TabsContent value="Stake History" className="sm:mt-6 mt-32 mb-72">
+              <Card className="p-4 shadow-md rounded-2xl">
+                <Table>
+                  <TableCaption className="text-muted-foreground mb-5">
+                    A list of placeholder invoices.
+                  </TableCaption>
+                  <TableHeader>
+                    <TableRow className="bg-muted/30">
+                      <TableHead className="w-[120px] font-semibold">#</TableHead>
+                      <TableHead className="font-semibold">title</TableHead>
+                      <TableHead className="font-semibold">Seller</TableHead>
+                      <TableHead className="font-semibold">Amount</TableHead>
+                      <TableHead className="text-right font-semibold">Status</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow className="hover:bg-muted/20 transition">
+                      <TableCell className="w-[120px] font-semibold"> 1</TableCell>
+                      <TableCell className="font-semibold"></TableCell>
+                      <TableCell className="font-semibold">0x2D105F67EB276145f839153Dec67E46554a32Ea6 </TableCell>
+                      <TableCell className="font-semibold">50 </TableCell>
+                      <TableCell className="text-right font-semibold">completed </TableCell>
                     </TableRow>
 
-                  ))
-                }
+                  </TableBody>
+                  <TableFooter>
+                    <TableRow className="bg-muted/30 font-medium">
 
-
-
-              </TableBody>
-              <TableFooter>
-                <TableRow className="bg-muted/30 font-medium">
-
-                </TableRow>
-              </TableFooter>
-            </Table>
-          </Card>
-        </TabsContent>
-
-        {/* Password Tab */}
-        <TabsContent value="My Votes" className="sm:mt-6 mt-32">
-          <Card className="p-4 shadow-md rounded-2xl">
-            <Table>
-              <TableCaption className="text-muted-foreground">
-                A list of placeholder invoices.
-              </TableCaption>
-              <TableHeader>
-                <TableRow className="bg-muted/30">
-                  <TableHead className="w-[120px] font-semibold">#</TableHead>
-                  <TableHead className="font-semibold">title</TableHead>
-                  <TableHead className="font-semibold">Seller</TableHead>
-                  <TableHead className="font-semibold">Amount</TableHead>
-                  <TableHead className="text-right font-semibold">Status</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow className="hover:bg-muted/20 transition">
-                  <TableCell className="w-[120px] font-semibold"> 1</TableCell>
-                  <TableCell className="font-semibold"></TableCell>
-                  <TableCell className="font-semibold">0x2D105F67EB276145f839153Dec67E46554a32Ea6 </TableCell>
-                  <TableCell className="font-semibold">50 </TableCell>
-                  <TableCell className="text-right font-semibold">completed </TableCell>
-                </TableRow>
-
-              </TableBody>
-              <TableFooter>
-                <TableRow className="bg-muted/30 font-medium">
-
-                </TableRow>
-              </TableFooter>
-            </Table>
-          </Card>
-        </TabsContent>
-
-        <TabsContent value="Stake History" className="sm:mt-6 mt-32">
-          <Card className="p-4 shadow-md rounded-2xl">
-            <Table>
-              <TableCaption className="text-muted-foreground">
-                A list of placeholder invoices.
-              </TableCaption>
-              <TableHeader>
-                <TableRow className="bg-muted/30">
-                  <TableHead className="w-[120px] font-semibold">#</TableHead>
-                  <TableHead className="font-semibold">title</TableHead>
-                  <TableHead className="font-semibold">Seller</TableHead>
-                  <TableHead className="font-semibold">Amount</TableHead>
-                  <TableHead className="text-right font-semibold">Status</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow className="hover:bg-muted/20 transition">
-                  <TableCell className="w-[120px] font-semibold"> 1</TableCell>
-                  <TableCell className="font-semibold"></TableCell>
-                  <TableCell className="font-semibold">0x2D105F67EB276145f839153Dec67E46554a32Ea6 </TableCell>
-                  <TableCell className="font-semibold">50 </TableCell>
-                  <TableCell className="text-right font-semibold">completed </TableCell>
-                </TableRow>
-
-              </TableBody>
-              <TableFooter>
-                <TableRow className="bg-muted/30 font-medium">
-
-                </TableRow>
-              </TableFooter>
-            </Table>
-          </Card>
-        </TabsContent>
+                    </TableRow>
+                  </TableFooter>
+                </Table>
+              </Card>
+            </TabsContent>
+          </>
+        )
+        }
 
       </Tabs>
     </div>
