65191be7a4138c63f1bb685fc533c058713badfd
Smitbhuva15 <itssmit123@gmail.com>
2025-09-23 17:07:03 +0530

    feat(chart): create deal flow visualization

--- escrow/src/components/Guides/GuidSection1.tsx
+++ escrow/src/components/Guides/GuidSection1.tsx
@@ -13,6 +13,17 @@ const GuidSection1 = () => {
                     Welcome to Escrow! Explore this guide to learn how to stake, vote, and resolve disputes with confidence.
                 </p>
                 <div className='border my-7 border-e-white'></div>
+                <h2 className='text-white text-2xl md:text-4xl font-bold my-4'>Deals Flow</h2>
+                <div className='flex justify-center'>
+                    <Image
+                        src="/DealFlowChart.png"
+                        alt="Deals Flow"
+                        width={850}
+                        height={768}
+                        className='mb-10 '
+                    />
+                </div>
+                <div className='border my-7 border-e-white'></div>
                 <div className=' text-white md:text-xl space-y-4 leading-relaxed my-6'>
                     <Guide1 />
                     <Guide2 />
